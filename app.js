cfa07725f67b2c50d6ebdad73ad801f7e2c5316d
egorfaresto <egorfaresto@gmail.com>
2022-09-05 18:33:36 +0600

    complete cards, todo delete tasks from lists

--- app.js
+++ app.js
@@ -62,6 +62,7 @@ menuItems.forEach((item, index) => {
                 contentSections[1].style.transform = `translateX(-${transformLength}%)`
                 contentSections[2].style.transform = `translateX(-300%)`
             } else if (index === 1) {
+                createUserTasksPage()
                 contentSections[2].style.transform = `translateX(-${transformLength}%)`
                 contentSections[1].style.transform = `translateX(-200%)`
             }
@@ -383,6 +384,7 @@ loginBtn.addEventListener('click', () => {
         changeUserData(checkThisUser)
         controlAccountPage()
         addTaskToCard()
+        createUserTasksPage()
         // applyTask(true)
     } else {
         loginInputs[0].value = ''
@@ -665,65 +667,6 @@ function checkCardIsTaken(card) {
 const applyTaskBtn = document.createElement('a')
 applyTaskBtn.classList.add('apply-task-btn')
 applyTaskBtn.textContent = 'Apply'
-//
-// function applyTask(firstCall) {
-//     // userLogged = checkUserLogged()
-//
-//     let currentUser = JSON.parse(sessionStorage.getItem('user'))
-//     let takenCards = [];
-//
-//     function cardActions(firstCall) {
-//         if (checkUserLogged() && currentUser.role !== 'admin') {
-//
-//
-//             for (let i = 1; i < cardsWrapper.childNodes.length; i++) {
-//                 let card = cardsWrapper.childNodes[i]
-//                 let taskNumber = card.querySelector('.card-item-left-sub-xp').textContent
-//                 let currentTask = Number(taskNumber.slice(13, taskNumber.length).trim())
-//                 let cardIsTaken = checkCardIsTaken(card)
-//
-//                 if (cardIsTaken && !takenCards.includes(card)) {
-//                     takenCards.push(card)
-//                     if (firstCall) {
-//                         changeCardColor(card)
-//                     }
-//                 }
-//                 if (!takenCards.includes(card)) {
-//                     card.addEventListener('mouseover', (e) => {
-//                         if (!takenCards.includes(card)) {
-//                             card.appendChild(applyTaskBtn)
-//                         }
-//                     })
-//
-//                     card.addEventListener('mouseleave', () => {
-//                         if (card !== null && card.contains(applyTaskBtn)) {
-//                             card.removeChild(applyTaskBtn)
-//                         }
-//                     })
-//                     card.addEventListener('click', (e) => {
-//                         if (e.target === applyTaskBtn) {
-//                             card = e.target.parentNode
-//                             // card.removeChild(applyTaskBtn)
-//                             for (let obj of cardsArr) {
-//                                 if (obj.taskId === currentTask) {
-//                                     changeCardColor(card, false)
-//                                     currentUser.tasks.push(currentTask)
-//                                     sessionStorage.setItem('user', JSON.stringify(currentUser))
-//                                     localStorage.setItem(`${currentUser.id}`, JSON.stringify(currentUser))
-//                                     break
-//                                 }
-//                             }
-//                             takenCards.push(card)
-//                             changeCardColor(card, false)
-//                             cardActions(false)
-//                         }
-//                     })
-//                 }
-//             }
-//         }
-//     }
-//     cardActions(firstCall)
-// }
 
 function addTaskToCard() {
     let currentUser = JSON.parse(sessionStorage.getItem('user'))
@@ -793,56 +736,122 @@ function restoreCards() {
 
 
 sessionStorage.clear()
-let rndUsr = localStorage.getItem('2')
-sessionStorage.setItem('user', rndUsr)
+// let rndUsr = localStorage.getItem('2')
+// sessionStorage.setItem('user', rndUsr)
 
 //region slider
+function createUserTasksPage() {
+    let currentUserTasks = JSON.parse(sessionStorage.getItem('user')).tasks
+    if (currentUserTasks.length > 0) {
 
-const userTasksPage = document.querySelector('.user__tasks-wrapper'),
-    slider = userTasksPage.querySelector('.slider__wrapper'),
-    sliderLeftBtn = userTasksPage.querySelector('.slide-left'),
-    sliderRightBtn = userTasksPage.querySelector('.slide-right'),
-    userTasksCards = userTasksPage.querySelectorAll('.user__task-card')
+        const userTasksPage = document.querySelector('.user__tasks-wrapper'),
+            slider = userTasksPage.querySelector('.slider__wrapper')
 
-let commonCardWidth = (parseFloat(userTasksCards[0].clientWidth) + 50) / 2
-let translateNum = 0
-let indexActive = translateNum / commonCardWidth
-userTasksCards[0].classList.add('active')
-sliderRightBtn.addEventListener('click', () => {
-    if (indexActive <= userTasksCards.length - 2) {
-        translateNum -= commonCardWidth
-        indexActive += 1
-        console.log(translateNum, 'width')
-        console.log(indexActive)
-        for (let card of userTasksCards) {
-            if (card !== undefined) {
-                card.classList.remove('active')
+        slider.innerHTML = ''
+
+
+        function CreateUserTasks(cardObject) {
+            this.id = cardObject.taskId
+            this.title = cardObject.title
+            this.text = cardObject.text
+        }
+
+        function createTaskCard(task) {
+            console.log(task)
+            const userTaskCard = document.createElement('div'),
+                userTaskTitle = document.createElement('h3'),
+                userTaskText = document.createElement('p'),
+                userTaskBtn = document.createElement('a')
+
+            userTaskCard.classList.add('user__task-card')
+            userTaskTitle.classList.add('user__task-title')
+            userTaskText.classList.add('user__task-text')
+            userTaskBtn.classList.add('user__task-btn')
+
+            userTaskTitle.textContent = `${task.title}`
+            userTaskText.innerHTML = `<span class="task__number">${task.id}</span><br><span>Описание задачи:</span> ${task.text}`
+            userTaskBtn.textContent = 'Завершить'
+
+            userTaskCard.appendChild(userTaskTitle)
+            userTaskCard.appendChild(userTaskText)
+            userTaskCard.appendChild(userTaskBtn)
+
+            return userTaskCard
+        }
+
+        for (let task of currentUserTasks) {
+            setTimeout(() => {
+                for (let item of cardsArr) {
+                    if (task === item.taskId) {
+                        let userTask = new CreateUserTasks(item)
+                        let currentUserTask = createTaskCard(userTask)
+                        slider.appendChild(currentUserTask)
+                        break
+                    }
+                }
+            }, 200)
+        }
+
+        setTimeout(() => {
+            enableSlider()
+        }, 300)
+
+        function enableSlider() {
+            const sliderLeftBtn = userTasksPage.querySelector('.slide-left'),
+                sliderRightBtn = userTasksPage.querySelector('.slide-right'),
+                userTasksCards = userTasksPage.querySelectorAll('.user__task-card')
+            let commonCardWidth = (parseFloat(userTasksCards[0].clientWidth) + 50) / 2
+            let translateNum = 0
+            let indexActive = translateNum / commonCardWidth
+            userTasksCards[0].classList.add('active')
+            sliderRightBtn.addEventListener('click', () => {
+                if (indexActive <= userTasksCards.length - 2) {
+                    translateNum -= commonCardWidth
+                    indexActive += 1
+                    // console.log(translateNum, 'width')
+                    // console.log(indexActive)
+                    for (let card of userTasksCards) {
+                        if (card !== undefined) {
+                            card.classList.remove('active')
+                        }
+                    }
+                    userTasksCards[indexActive].classList.add('active')
+                    slider.style.transform = `translateX(${translateNum}px)`
+                }
+            })
+
+            sliderLeftBtn.addEventListener('click', () => {
+                if (translateNum < 0) {
+                    translateNum += commonCardWidth
+                    indexActive -= indexActive > 0 ? 1 : 0
+                    // console.log(indexActive)
+                    // console.log(translateNum, 'width')
+                    for (let card of userTasksCards) {
+                        if (card !== undefined) {
+                            card.classList.remove('active')
+                        }
+                    }
+                    if (indexActive >= 0) {
+                        userTasksCards[indexActive].classList.add('active')
+                        slider.style.transform = `translateX(${translateNum}px)`
+                    }
+                }
+            })
+        }
+
+        slider.addEventListener('click', (e) => {
+            if (e.target.classList.contains('user__task-btn')) {
+                console.log(e.target.parentNode)
+                e.target.parentNode.style.opacity = '0'
+                setTimeout(() => {
+                    e.target.parentNode.style.display = 'none'
+                    let currentUser = JSON.parse(sessionStorage.getItem('user'))
+                    for (let part of e.target.childNodes) {
+
+                    }
+                    // currentUser.tasks.indexOf()
+                }, 500)
             }
-        }
-        userTasksCards[indexActive].classList.add('active')
-        slider.style.transform = `translateX(${translateNum}px)`
+        })
     }
-})
-
-
-sliderLeftBtn.addEventListener('click', () => {
-    if (translateNum < 0) {
-        translateNum += commonCardWidth
-        indexActive -= indexActive > 0 ? 1 : 0
-        console.log(indexActive)
-        console.log(translateNum, 'width')
-        for (let card of userTasksCards) {
-            if (card !== undefined) {
-                card.classList.remove('active')
-            }
-        }
-        if (indexActive >= 0) {
-            userTasksCards[indexActive].classList.add('active')
-            slider.style.transform = `translateX(${translateNum}px)`
-        }
-    }
-})
-
-// function CreateUserTasks() {
-//     this.
-// }
+}
